539136966793ab7abcf6b9604eb757e607b04f4b
JISHIRO <kronosiraxx@gmail.com>
2023-09-30 10:42:57 -0500

    Ejercicio Comont Terminado

--- Parcial 2/Modulo_Comont/Area.js
+++ Parcial 2/Modulo_Comont/Area.js
@@ -1,5 +1,5 @@
-
-function areaCuadrado(lado){
+// EJERCICIO EN CLASE
+/*function areaCuadrado(lado){
     return lado*lado;
 }
 function areaTriangulo(base,altura){
@@ -9,7 +9,28 @@ function areaTriangulo(base,altura){
 module.exports.areaCuadrado=areaCuadrado;
 module.exports.areaTriangulo=areaTriangulo;
 
-/*console.log(__dirname);
-console.log(__filename);*/
-console.log(module);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);*/
 
+
+//Ejercicio Propio
+/*function saludarPersona(nombre) {
+    return `Hola, ${nombre}!`;
+}  
+module.exports = saludarPersona;*/
+
+//Ejercicio propio 2
+class Person {
+    constructor(nombre, edad) {
+      this.nombre = nombre;
+      this.edad = edad;
+    }
+  
+    saludar() {
+      console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
+    }
+  }
+  // Exportar la clase para que otros módulos puedan usarla
+  module.exports = Person;
+  
