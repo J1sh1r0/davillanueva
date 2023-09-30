388b19cdc62f35e0454cb615f69f19dadc9db145
JISHIRO <kronosiraxx@gmail.com>
2023-09-30 11:33:09 -0500

    Reacomodo

--- Parcial 2/Modulo_Comont/Area.js
+++ Parcial 2/Modulo_Comont/Area.js
@@ -1,5 +1,5 @@
 // EJERCICIO EN CLASE
-/*function areaCuadrado(lado){
+function areaCuadrado(lado){
     return lado*lado;
 }
 function areaTriangulo(base,altura){
@@ -11,26 +11,4 @@ module.exports.areaTriangulo=areaTriangulo;
 
 console.log(__dirname);
 console.log(__filename);
-console.log(module);*/
-
-
-//Ejercicio Propio
-/*function saludarPersona(nombre) {
-    return `Hola, ${nombre}!`;
-}  
-module.exports = saludarPersona;*/
-
-//Ejercicio propio 2
-class Person {
-    constructor(nombre, edad) {
-      this.nombre = nombre;
-      this.edad = edad;
-    }
-  
-    saludar() {
-      console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} años.`);
-    }
-  }
-  // Exportar la clase para que otros módulos puedan usarla
-  module.exports = Person;
-  
+console.log(module);
--- Parcial 2/Modulo_Comont/CalculoAreas.js
+++ Parcial 2/Modulo_Comont/CalculoAreas.js
@@ -1,17 +1,4 @@
 //Ejercicio de clase
-//let areas=require("./Area.js");
-//let a=areas.areaCuadrado(4);
-//console.log(a);
-
-
-//EJERCICIO PROPIO 1
-/*const saludarPersona = require('./Area.js');
-const nombre = 'Dominic';
-const saludo = saludarPersona(nombre);
-console.log(saludo);*/
-
-//EJERCICIO PROPIO 2
-const Person = require('./Area.js');
-const persona = new Person('Dominic', 21); // Crear una instancia de la clase Person
-persona.saludar();// Llamar al método saludar
-
+let areas=require("./Area.js");
+let a=areas.areaCuadrado(4);
+console.log(a);
