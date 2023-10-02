b0d657cfc0239ff82aef02b39073c304f87382b9
JISHIRO <kronosiraxx@gmail.com>
2023-10-02 07:12:10 -0500

    Cambios Modulo Comont 2

--- Parcial 2/Modulo_CommonJS/Mensaje.js
+++ Parcial 2/Modulo_CommonJS/Mensaje.js
@@ -5,6 +5,16 @@ const saludo = saludarPersona(nombre);
 console.log(saludo);*/
 
 //EJERCICIO PROPIO 2
-const Person = require('./Person.js');
-const persona = new Person('Dominic', 21); // Crear una instancia de la clase Person
-persona.saludar();// Llamar al método saludar
+//const Person = require('./Person.js');
+//const persona = new Person('Dominic', 21); // Crear una instancia de la clase Person
+//persona.saludar();// Llamar al método saludar
+
+// Importar el módulo
+/*const modulo = require('./Person.js');
+
+// Utilizar las funciones importadas
+const resultadoSuma = modulo.sumar(5, 3);
+const resultadoResta = modulo.restar(10, 4);
+
+console.log(`La suma es: ${resultadoSuma}`);
+console.log(`La resta es: ${resultadoResta}`);*/
--- Parcial 2/Modulo_CommonJS/Person.js
+++ Parcial 2/Modulo_CommonJS/Person.js
@@ -5,7 +5,7 @@
 module.exports = saludarPersona;*/
 
 //Ejercicio propio 2
-class Person {
+/*class Person {
     constructor(nombre, edad) {
       this.nombre = nombre;
       this.edad = edad;
@@ -16,4 +16,35 @@ class Person {
     }
   }
   // Exportar la clase para que otros módulos puedan usarla
-  module.exports = Person;
+  module.exports = Person;*/
+
+  //EJERCICIO 3
+  // Definir una función que suma dos números
+/*function sumar(a, b) {
+  return a + b;
+}
+
+// Definir una función que resta dos números
+function restar(a, b) {
+  return a - b;
+}
+
+// Exportar las funciones
+module.exports.sumar = sumar;
+module.exports.restar = restar;*/
+
+
+
+// Importa el módulo axios
+const axios = require('axios');
+// URL de ejemplo para hacer una petición GET
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+// Realiza una petición GET a la URL
+axios.get(url)
+  .then(response => {
+    console.log('Datos recibidos:', response.data);
+  })
+
+  .catch(error => {
+    console.error('Error al hacer la petición:', error);
+  });
